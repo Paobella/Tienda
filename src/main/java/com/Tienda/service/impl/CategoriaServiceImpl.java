26114ceff31cc199c659bd78d4949a621bfd49e9
Paola Segura <145728086+Paobella@users.noreply.github.com>
2024-02-27 15:19:27 -0600

    Finalización de semana 6

--- src/main/java/com/Tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/Tienda/service/impl/CategoriaServiceImpl.java
@@ -1,9 +1,8 @@
-
 package com.Tienda.service.impl;
 
 import com.Tienda.dao.CategoriaDao;
 import com.Tienda.domain.Categoria;
-import com.Tienda.service.CategoriaService;
+import com.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,26 +10,35 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
-    
+
     @Autowired
     private CategoriaDao categoriaDao;
 
     @Override
     @Transactional(readOnly = true)
-    public List<Categoria> getCategorias(boolean activo) {
-        List<Categoria> lista = categoriaDao.findAll();
-        
-        if (activo) {
-            //Remover los elementos inactivos
-            lista.removeIf(c -> !c.isActivo());
-            
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista = categoriaDao.findAll();
+        if (activos) {
+            lista.removeIf(e -> !e.isActivo());
         }
-        
-        
-        
         return lista;
-        
     }
-    
-    
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
 }
